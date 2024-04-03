2ff6d291a681942c70b3c116b3833ce52bf0f0e6
funkim <mcking172@skiff.com>
2024-04-03 17:06:37 -0400

    Set up the base for the Game

---
+++ script.js
@@ -0,0 +1,9 @@
+const TikTacToe = (function() {
+    let board = ['','','','','','','','',''];
+    const Player = (name, symbol) => {
+    return { name, symbol };
+    const playerX = Player('Player 1', 'X');
+    const playerO = Player('Player 2', 'O');
+};
+
+}) 
